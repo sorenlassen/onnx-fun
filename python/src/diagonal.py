804a3ac50c4ca111537b2b2a319ff74958b029cc
Soren Lassen <sorenlassen@gmail.com>
2022-04-26 08:00:43 -0700

    factored out arg checking and output shape calculation

--- python/src/diagonal.py
+++ python/src/diagonal.py
@@ -3,6 +3,8 @@ import numpy as np
 import onnx
 import onnxruntime
 
+
+# onnx helpers
 Numpy2OnnxType = {
         np.float32: onnx.TensorProto.FLOAT,
         np.float64: onnx.TensorProto.DOUBLE,
@@ -21,44 +23,54 @@ def run_model(model, output_names, input_names, inputs):
     sess = onnxruntime.InferenceSession(model.SerializeToString())
     return sess.run(output_names, dict(zip(input_names, inputs)))
 
-ASCII_LETTERS = list(string.ascii_uppercase + string.ascii_lowercase)
 
-# Implements np.diagonal (without offset) with onnx.
-def diagonal_by_einsum(data, axis1=0, axis2=1):
+# diagonal helpers:
+
+# Checks input args. Returns non-negative axes and the output shape.
+def diagonal_check_arguments(data, offset=0, axis1=0, axis2=1):
     assert data.ndim >= 2
     assert all(-data.ndim <= x < data.ndim for x in [axis1, axis2])
     if axis1 < 0: axis1 += data.ndim
     if axis2 < 0: axis2 += data.ndim
+    assert axis1 != axis2, f"{axis1},{axis2}"
     assert data.shape[axis1] == data.shape[axis2]
-    assert axis1 != axis2
-    fst, snd = min(axis1, axis2), max(axis1, axis2)
+    oshape = list(data.shape) + [data.shape[axis1]]
+    for ax in reversed(sorted([axis1, axis2])):
+        del oshape[ax]
+    return axis1, axis2, tuple(oshape)
+
+
+# einsum helpers
+ASCII_LETTERS = list(string.ascii_uppercase + string.ascii_lowercase)
+
+# Implements np.diagonal (without offset) with onnx.
+def diagonal_by_einsum(data, offset=0, axis1=0, axis2=1):
+    assert offset == 0
+    axis1, axis2, oshape = diagonal_check_arguments(data, axis1=axis1, axis2=axis2)
     assert data.ndim <= len(ASCII_LETTERS), \
             "ran out of letters for indices, shape is too long"
     osubscripts = ASCII_LETTERS[:data.ndim - 1]
     isubscripts = osubscripts[:-1]
-    isubscripts.insert(fst, osubscripts[-1])
-    isubscripts.insert(snd, osubscripts[-1])
+    for ax in sorted([axis1, axis2]):
+        isubscripts.insert(ax, osubscripts[-1])
     equation = f"{''.join(isubscripts)}->{''.join(osubscripts)}"
     node = onnx.helper.make_node(
             "Einsum",
             inputs=["data"],
             outputs=["diag"],
             equation=equation)
-    in_shape = data.shape
-    out_shape = in_shape[:fst] + in_shape[fst + 1:snd] + in_shape[snd + 1:] + (in_shape[fst],)
-    dtype = data.dtype
     model = onnx.helper.make_model(
             graph=onnx.helper.make_graph(
                 name='diagonal_by_einsum',
                 nodes=[node],
-                inputs=[param(node.input[0], dtype, in_shape)],
-                outputs=[param(node.output[0], dtype, out_shape)],
+                inputs=[param(node.input[0], data.dtype, data.shape)],
+                outputs=[param(node.output[0], data.dtype, oshape)],
                 )
             )
     onnx.checker.check_model(model)
     [diag] = run_model(model, node.output, node.input, [data])
     assert data.dtype == diag.dtype
-    assert out_shape == diag.shape
+    assert oshape == diag.shape
     return diag
 
 def diagonal_by_einsum_test():
@@ -72,7 +84,7 @@ def diagonal_by_einsum_test():
             (np.random.rand(2,1,3,2), 0, 3),
             ]:
         expected = np.diagonal(data, axis1=axis1, axis2=axis2)
-        actual = diagonal_by_einsum(data, axis1, axis2)
+        actual = diagonal_by_einsum(data, axis1=axis1, axis2=axis2)
         assert expected.dtype == actual.dtype
         assert expected.shape == actual.shape, f"{expected.shape},{actual.shape}"
         assert np.allclose(expected, actual)
