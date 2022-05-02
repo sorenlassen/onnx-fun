f12ae533b347572d87f090f5e694773faa6e1c24
Soren Lassen <sorenlassen@gmail.com>
2022-05-02 16:42:28 -0700

    introduced Transform data struct
    and implemented reducesum

--- python/src/einsum_onnx.py
+++ python/src/einsum_onnx.py
@@ -1,8 +1,10 @@
+from dataclasses import dataclass
+from typing import List, Tuple, Union
 import string
 import numpy as np
-import onnx
-import onnxruntime
-import einsum
+import onnx # type: ignore
+import onnxruntime # type: ignore
+import einsum # type: ignore
 
 
 # EinsumSpec helpers
@@ -70,12 +72,15 @@ def make_constant_model(graph_name, output_name, tensor):
                 )
             )
 
+def make_identity_node(input_name, output_name):
+    return onnx.helper.make_node(
+            "Identity",
+            inputs=[input_name],
+            outputs=[output_name],
+        )
+
 def make_identity_model(graph_name, input_name, output_name, dtype, shape):
-    identity_node = onnx.helper.make_node(
-        "Identity",
-        inputs=[input_name],
-        outputs=[output_name],
-    )
+    identity_node = make_identity_node(input_name, output_name)
     return onnx.helper.make_model(
             graph=onnx.helper.make_graph(
                 name=graph_name,
@@ -84,24 +89,6 @@ def make_identity_model(graph_name, input_name, output_name, dtype, shape):
                 outputs=[param(output_name, dtype, shape)],
                 )
             )
-
-def make_squeeze_model(graph_name, input_name, output_name, dtype, shape, axes):
-    axes_node = make_constant_node(graph_name, np.array(axes, dtype=np.int64))
-    squeeze_node = onnx.helper.make_node(
-        "Squeeze",
-        inputs=[input_name, graph_name],
-        outputs=[output_name],
-    )
-    oshape = squeeze_shape(shape, axes)
-    model = onnx.helper.make_model(
-            graph=onnx.helper.make_graph(
-                name=graph_name,
-                nodes=[axes_node, squeeze_node],
-                inputs=[param(input_name, dtype, shape)],
-                outputs=[param(output_name, dtype, oshape)],
-                )
-            )
-    return model, oshape
 
 def run_model(model, *inputs):
     sess = onnxruntime.InferenceSession(model.SerializeToString())
@@ -117,6 +104,74 @@ def infer_shapes_and_run_model(model, *inputs):
     model = onnx.shape_inference.infer_shapes(model)
     onnx.checker.check_model(model)
     return run_model(model, *inputs)
+
+
+@dataclass
+class Transform:
+    iname: str
+    ishape: Tuple[int, ...]
+    dtype: Union[np.dtype, type] # e.g. np.type(np.int32) or np.int32
+    oname: str
+    oshape: Tuple[int, ...]
+    nodes: List[onnx.onnx_ml_pb2.NodeProto]
+
+    def graph(self, graph_name):
+        if len(self.nodes) == 0:
+            # Empty graphs don't come compose with onnx.compose, so
+            # we insert an Identity node for robustness.
+            final_oname = f"{graph_name}/out"
+            final_nodes = [make_identity_node(self.iname, final_oname)]
+        else:
+            final_oname = self.oname
+            final_nodes = self.nodes
+        return onnx.helper.make_graph(
+                name=graph_name,
+                nodes=final_nodes,
+                inputs=[param(self.iname, self.dtype, self.ishape)],
+                outputs=[param(final_oname, self.dtype, self.oshape)],
+                )
+
+    def model(self, graph_name):
+        return onnx.helper.make_model(graph = self.graph(graph_name))
+
+    def next_name(self, stem):
+        return f"{self.iname}/{stem}{len(self.nodes)}"
+
+    def squeeze(self, axes):
+        if len(axes) == 0:
+            return self
+        axes_tensor = np.array(axes, dtype=np.int64)
+        axes_name = self.next_name("axes")
+        self.nodes.append(make_constant_node(axes_name, axes_tensor))
+        squeeze_name = self.next_name("squeeze")
+        self.nodes.append(onnx.helper.make_node(
+            "Squeeze",
+            inputs=[self.oname, axes_name],
+            outputs=[squeeze_name],
+        ))
+        self.oname = squeeze_name
+        self.oshape = squeeze_shape(self.oshape, axes)
+        return self
+
+    def reducesum(self, axes):
+        if len(axes) == 0:
+            return self
+        axes_tensor = np.array(axes, dtype=np.int64)
+        axes_name = self.next_name("axes")
+        self.nodes.append(make_constant_node(axes_name, axes_tensor))
+        sum_name = self.next_name("sum")
+        self.nodes.append(onnx.helper.make_node(
+            "ReduceSum",
+            inputs=[self.oname, axes_name],
+            outputs=[sum_name],
+            keepdims=0,
+        ))
+        self.oname = sum_name
+        self.oshape = squeeze_shape(self.oshape, axes)
+        return self
+
+def make_identity_transform(dtype, shape, iname):
+    return Transform(iname, shape, dtype, iname, shape, [])
 
 
 def einsum_direct_model(equation, ishapes, dtype):
@@ -160,8 +215,6 @@ def einsum_direct_model_test():
 
 def einsum_decomposed_model(equation, ishapes, dtype):
     spec = einsum.einsum_spec(equation, ishapes)
-    ninputs = len(ishapes)
-    input_names = [f"in{i}" for i in range(ninputs)]
 
     # In two cases the output is just zeros or empty:
     # (1) empty if there are any 0 dims in the output shape,
@@ -176,22 +229,28 @@ def einsum_decomposed_model(equation, ishapes, dtype):
     # Each transform is either an onnx model transforming the input
     # at that position or just a string with the name of the input
     # which represents the identity transformation.
-    transforms = list(input_names)
+    ninputs = len(ishapes)
+    transforms = [
+            make_identity_transform(dtype, ishapes[i], f"in{i}")
+            for i in range(ninputs)
+            ]
 
     for i in range(ninputs):
-        spec, transforms = einsum_diagonalize_input(spec, transforms, i, dtype)
-        spec, transforms = einsum_reducesum_input(spec, transforms, i, dtype)
+        spec, transforms = einsum_diagonalize_input(spec, transforms, i)
+        spec, transforms = einsum_reducesum_input(spec, transforms, i)
 
     while len(transforms) > 1:
-        spec, transforms = einsum_contract_inputs(spec, transforms, 0, 1, dtype)
+        spec, transforms = einsum_contract_inputs(spec, transforms, 0, 1)
 
-    return einsum_finalize(spec, transforms[0], dtype)
+    transform = einsum_finalize(spec, transforms[0])
+    #print("final transform",transform)
+    return transform.model(f"einsum({equation})")
 
-def einsum_diagonalize_input(spec, transforms, i, dtype):
+def einsum_diagonalize_input(spec, transforms, i):
     # TODO: implement
     return spec, transforms
 
-def einsum_reducesum_input(spec, transforms, i, dtype):
+def einsum_reducesum_input(spec, transforms, i):
     ispec = spec.inputs[i]
     idxs = list(ispec.idxs)
     shape = list(ispec.shape)
@@ -211,64 +270,32 @@ def einsum_reducesum_input(spec, transforms, i, dtype):
         if shape[axis] == 1:
             one_axes.append(axis)
             one_idxs.append(idx)
-    transforms[i], shape = squeeze_output(transforms[i], one_axes, shape, dtype)
+    transforms[i].squeeze(one_axes)
+    for a in sorted(one_axes, reverse=True):
+        del idxs[a]
+        del shape[a]
+    assert tuple(shape) == transforms[i].oshape
 
     # ReduceSum the rest.
     for idx in idxs_only_in_i - set(one_idxs):
         axis = idxs.index(idx)
         assert shape[axis] > 1
-        transforms[i], shape = reducesum_output(transforms[i], axis, shape, dtype)
+        transforms[i].reducesum([axis])
         del idxs[axis]
+        del shape[axis]
+        assert tuple(shape) == transforms[i].oshape
 
     ispec.idxs = idxs
     ispec.shape = tuple(shape)
     return spec, transforms
 
-def transform_input_name(transform):
-    if isinstance(transform, str):
-        return transform
-    else:
-        return transform.graph.input[0].name
-
-def transform_output_name(transform):
-    if isinstance(transform, str):
-        return transform
-    else:
-        return transform.graph.output[0].name
-
-def squeeze_output(transform, axes, shape, dtype):
-    if len(axes) == 0:
-        return transform, shape
-    graph_name = "squeeze" # TODO: make this unique
-    iname = transform_input_name(transform)
-    squeeze_model, oshape = make_squeeze_model(
-            graph_name, iname, "out", dtype, shape, axes)
-    if isinstance(transform, str):
-        transform = squeeze_model
-    else:
-        oname = transform_output_name(transform)
-        transform = onnx.compose.merge_models(
-                transform, squeeze_model,
-                io_map=[(oname, iname)])
-    return transform, oshape
-
-def reducesum_output(transform, axis, shape, dtype):
-    # TODO: implement
-    oshape = list(shape)
-    del oshape[axis]
-    return transform, oshape
-
 def einsum_contract_inputs(spec, transforms, i, j, dtype):
     # TODO: implement
     return spec, transforms
 
-def einsum_finalize(spec, transform, dtype):
+def einsum_finalize(spec, transform):
     if einsum_is_identity_spec(spec):
         # The equation is the identity transformation.
-        if isinstance(transform, str):
-            return make_identity_model(
-                    'einsum_identity', transform, "out",
-                    dtype, spec.output.shape)
         return transform
     # TODO: transpose and/or reshape
     return transform
@@ -290,6 +317,11 @@ def einsum_decomposed_model_test():
             ("isj->ij", [(2,1,3)]),
             ("ijs->ij", [(2,3,1)]),
             ("sitju->ij", [(1,2,1,3,1)]),
+            # reducesum axes s,t,u:
+            ("sij->ij", [(4,2,3)]),
+            ("isj->ij", [(2,4,3)]),
+            ("ijs->ij", [(2,3,4)]),
+            ("sitju->ij", [(4,2,5,3,6)]),
             ]:
         inputs = [ np.random.rand(*shape) for shape in ishapes ]
         expected = np.einsum(equation, *inputs)
