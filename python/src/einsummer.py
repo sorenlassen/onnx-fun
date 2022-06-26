f83d84ce50f53f158030c4ce869784e050996d3b
Soren Lassen <sorenlassen@gmail.com>
2022-06-26 14:09:02 -0700

    added ONNX helpers

--- python/src/einsummer.py
+++ python/src/einsummer.py
@@ -1,21 +1,83 @@
 from collections import defaultdict
 from dataclasses import dataclass
-from typing import Any, Dict, KeysView, List, Sequence, Tuple
+from typing import Any, Dict, KeysView, List, Sequence, Tuple, Union
 from copy import deepcopy
+import math
 import string
 import os
 import numpy as np
+import onnx # type: ignore
+import onnxruntime # type: ignore
 
 
-IntTuple = Tuple[int, ...]
-Shape = IntTuple
-DType = np.dtype
-OnnxNode = Any # TODO: describe OnnxNode type better
+Shape = Tuple[int, ...]
+DType = Union[np.dtype, type]
 
 
 VERBOSE = int(os.getenv("EINSUM_VERBOSE", "0")) > 0
 def log(*args):
     if VERBOSE: print(*args)
+
+
+# ONNX helpers:
+def onnx_type(dtype : DType) -> onnx.TensorProto:
+    '''Returns equivalent onnx.TensorProto basetype for a given NumPy type
+    where dtype can be either a np.dtype or np.float32, np.int64, etc.'''
+    ty = np.dtype(dtype) # np.dtype() is idempotent
+    return {
+        # TODO: support smaller int types (currently unsupported because of the type
+        # constraints of ONNX ReduceSum and MatMul which we use to decompose Einsum)
+        # TODO: support BFLOAT16 (currently unsupported by NumPy)
+        np.dtype(np.int32): onnx.TensorProto.INT32,
+        np.dtype(np.uint32): onnx.TensorProto.UINT32,
+        np.dtype(np.int64): onnx.TensorProto.INT64,
+        np.dtype(np.uint64): onnx.TensorProto.UINT64,
+        np.dtype(np.float16): onnx.TensorProto.FLOAT16,
+        np.dtype(np.float32): onnx.TensorProto.FLOAT,
+        np.dtype(np.float64): onnx.TensorProto.DOUBLE,
+    }[ty]
+
+def param(param_name: str, dtype: DType, shape: Shape) -> onnx.ValueInfoProto:
+    return onnx.helper.make_tensor_value_info(
+        param_name,
+        onnx_type(dtype),
+        shape)
+
+def make_constant_node(output_name: str, tensor) -> onnx.NodeProto:
+    tensor = np.asarray(tensor)
+    return onnx.helper.make_node(
+        "Constant",
+        inputs=[],
+        outputs=[output_name],
+        value=onnx.helper.make_tensor(
+            name=output_name,
+            data_type=onnx_type(tensor.dtype),
+            dims=tensor.shape,
+            vals=tensor.flatten(),
+        ),
+    )
+
+def make_typed_graph(graph_name, nodes, inputs, outputs, dtype) -> onnx.GraphProto:
+    return onnx.helper.make_graph(
+        name=graph_name,
+        nodes=nodes,
+        inputs=[param(name, dtype, shape) for name, shape in inputs],
+        outputs=[param(name, dtype, shape) for name, shape in outputs],
+    )
+
+def run_model(model: onnx.ModelProto, *inputs):
+    sess = onnxruntime.InferenceSession(model.SerializeToString())
+    def names(params): return map(lambda param: param.name, params)
+    # model might omit an input, e.g. when result is just a constant
+    assert len(model.graph.input) <= len(inputs)
+    inputs_dict = dict(zip(names(model.graph.input), inputs))
+    output_names = list(names(model.graph.output))
+    return sess.run(output_names, inputs_dict)
+
+def infer_shapes_and_run_model(model: onnx.ModelProto, *inputs):
+    model = onnx.shape_inference.infer_shapes(model)
+    onnx.checker.check_model(model)
+    return run_model(model, *inputs)
 
 
 EINSUM_ELLIPSIS = "..."
@@ -88,6 +150,9 @@ class EinsumParam:
     def rank(self) -> int:
         return len(self.shape)
 
+    def size(self) -> int:
+        return math.prod(self.shape)
+
     def delete(self, axis: int) -> None:
         assert -self.rank() <= axis < self.rank()
         del self.subscripts[axis]
@@ -105,7 +170,7 @@ class Einsummer:
     inputs: List[EinsumParam]
     outputs: List[EinsumParam]
     result: EinsumParam # result.name is ignored
-    nodes: List[OnnxNode]
+    nodes: List[onnx.NodeProto]
 
     def __init__(self, inputs: List[EinsumParam], result: EinsumParam, dtype: DType):
         self.dtype = dtype
@@ -151,6 +216,10 @@ class Einsummer:
         ...
 
     def transform(self) -> None:
+        if self.result.size() == 0 or any(ou.size() == 0 for ou in self.outputs):
+            # output is empty or all zeros (from ReduceSum of zero dim)
+            self.nodes.append(make_constant_node(self.result.name, np.zeros(self.result.shape)))
+            return
         for output in self.outputs:
             self.diagonalize(output)
             self.reduceSum(output)
@@ -160,13 +229,29 @@ class Einsummer:
 
 def einsummer_test():
     print("einsummer_test() start")
-    in1 = EinsumParam("in1", EinsumSubscripts("a...ij"), (2,1,2,3,3,2))
-    in2 = EinsumParam("in2", EinsumSubscripts("...jkk"), (5,1,3,2,4,4))
-    in3 = EinsumParam("in3", EinsumSubscripts("xyzx...xwx"), (2,2,1,1,1,2,2))
-    res = EinsumParam("res", EinsumSubscripts("ik"), (3,4))
-    ein = Einsummer([in1, in2, in3], res, DType(np.float32))
-    ein.transform()
-    log("ein:",ein)
+
+    in1_1 = EinsumParam("in1", EinsumSubscripts("a...ij"), (2,1,2,3,3,2))
+    in1_2 = EinsumParam("in2", EinsumSubscripts("...jkk"), (5,1,3,2,4,4))
+    in1_3 = EinsumParam("in3", EinsumSubscripts("xyzx...xwx"), (2,2,1,1,1,2,2))
+    res1 = EinsumParam("res", EinsumSubscripts("ik"), (3,4))
+    ein1 = Einsummer([in1_1, in1_2, in1_3], res1, np.float32)
+    ein1.transform()
+    log("ein1:",ein1)
+
+    # zeros result because of zero dim in input
+    in2 = EinsumParam("in", EinsumSubscripts("ij"), (0,2))
+    res2 = EinsumParam("res", EinsumSubscripts("j"), (2,))
+    ein2 = Einsummer([in2], res2, np.uint32)
+    ein2.transform()
+    log("ein2:",ein2)
+
+    # empty result because of zero dim in result
+    in3 = EinsumParam("in", EinsumSubscripts("ij"), (0,2))
+    res3 = EinsumParam("res", EinsumSubscripts("ji"), (2,0))
+    ein3 = Einsummer([in3], res3, float)
+    ein3.transform()
+    log("ein3:",ein3)
+
     print("einsummer_test() end")
 
 if __name__ == "__main__":
